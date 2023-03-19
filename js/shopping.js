a420c42f353ae60eefd344b8c98c3a5e56c4b794
noornabi07 <noornabiprogram07@gmail.com>
2023-03-19 08:09:34 +0600

    book shopping get and display done

---
+++ js/shopping.js
@@ -0,0 +1,32 @@
+const getData = async() =>{
+    const url = `https://fakestoreapi.com/products`;
+    const res = await fetch(url);
+    const data = await res.json();
+    showData(data);
+    // console.log(data)
+}
+
+const showData = datas =>{
+    const productContainer = document.getElementById('product-container');
+    datas.forEach(data =>{
+        const myDiv = document.createElement('div');
+        myDiv.innerHTML = `
+        <div class="card h-[550px] w-full bg-base-100 border shadow-xl">
+        <figure><img class="h-96" src="${data.image}" alt="Shoes" /></figure>
+        <div class="card-body text-center">
+          <h2 class="card-title font-bold text-5xl text-red-500">$${data.price}</h2>
+          <h3 class="card-title font-bold text-lime-500"><span class="text-yellow-500">Rating:</span> ${data.rating.rate}</h3>
+          <h4 class="card-title font-bold text-green-600"><span class="text-yellow-500">Catagories:</span> ${data.category}</h4>
+          <p class="text-3xl font-semibold">${data.title}</p>
+          <div class="card-actions justify-center">
+            <button class="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+        `;
+        productContainer.appendChild(myDiv);
+        console.log(data);
+    })
+}
+
+getData();
