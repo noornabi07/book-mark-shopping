d44e999549b25d7241e47bf63e8ffe194be9f8b4
noornabi07 <noornabiprogram07@gmail.com>
2023-03-19 09:34:11 +0600

    slider recreate done

--- js/shopping.js
+++ js/shopping.js
@@ -1,10 +1,11 @@
 // slider part
 const images = [
-    'images/pic-1.jpg',
-    'images/pic-2.jpg',
-    'images/pic-3.jpg',
-    'images/pic-4.jpg',
-    'images/pic-5.jpg'
+    'images/shirt-1.jpg',
+    'images/shirt-2.jpg',
+    'images/shirt-3.jpg',
+    'images/shirt-4.jpg',
+    'images/shirt-5.jpg',
+ 
 ]
 const startSlider = () =>{
     let imgCount = 0;
@@ -40,8 +41,17 @@ const showData = datas =>{
     datas.forEach(data =>{
         const myDiv = document.createElement('div');
         myDiv.innerHTML = `
-        <div class="card h-[550px] w-full bg-base-100 border shadow-xl">
-        <figure><img class="h-96" src="${data.image}" alt="Shoes" /></figure>
+        <div class="card h-[750px] w-full bg-base-100 border shadow-xl">
+
+        <figure>
+          <img class="h-96 z-10 relative" src="${data.image}" alt="Shoes" />
+
+          <div class="absolute top-px right-px p-3">
+          <i class="fa-solid fa-bookmark text-5xl z-20 mr-3 text-purple-500"></i>
+          <i class="fa-regular fa-bookmark text-5xl z-20 text-lime-600"></i>
+          </div>
+        </figure>
+
         <div class="card-body text-center">
           <h2 class="card-title font-bold text-5xl text-red-500">$${data.price}</h2>
           <h3 class="card-title font-bold text-lime-500"><span class="text-yellow-500">Rating:</span> ${data.rating.rate}</h3>
